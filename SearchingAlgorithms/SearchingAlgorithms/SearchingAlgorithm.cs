0733c7781dddb319ab9bfa90978a889f1bafcee4
VeselinDenchev <den4ev01@gmail.com>
2022-06-06 17:10:23 +0300

    Add searching algorithms
    Add Linear Search and Binary Search (both while-loop and recursive realization)

---
+++ SearchingAlgorithms/SearchingAlgorithms/SearchingAlgorithm.cs
@@ -0,0 +1,68 @@
+﻿namespace SearchingAlgorithms
+{
+    internal class SearchingAlgorithm
+    {
+        public static int? LinearSearch(int[] array, int desiredElement)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                bool isFound = array[index] == desiredElement;
+                if (isFound)
+                {
+                    return index;
+                }
+            }
+
+            return null;
+        }
+
+        public static int? BinarySearch(int[] sortedArray, int desiredElement)
+        {
+            int minNumberIndex = 0;
+            int maxNumberIndex = sortedArray.Length - 1;
+
+            while (minNumberIndex <= maxNumberIndex)
+            {
+                int middleNumberIndex = (minNumberIndex + maxNumberIndex) / 2;
+
+                bool isFound = desiredElement == sortedArray[middleNumberIndex];
+                if (isFound)
+                {
+                    return middleNumberIndex;
+                }
+                else if (desiredElement < sortedArray[middleNumberIndex])
+                {
+                    maxNumberIndex = middleNumberIndex - 1;
+                }
+                else
+                {
+                    minNumberIndex = middleNumberIndex + 1;
+                }
+            }
+
+            return null;
+        }
+
+        public static int? BinarySearchRecursive(int[] sortedArray, int minNumberIndex, int maxNumberIndex, int desiredNumber)
+        {
+            if (maxNumberIndex >= minNumberIndex)
+            {
+                int middleNumberIndex = minNumberIndex + (maxNumberIndex - minNumberIndex) / 2;
+
+                if (sortedArray[middleNumberIndex] == desiredNumber)
+                {
+                    return middleNumberIndex;
+                }
+
+                if (sortedArray[middleNumberIndex] > desiredNumber)
+                {
+                    return BinarySearchRecursive(sortedArray, minNumberIndex, middleNumberIndex - 1, desiredNumber);
+                }
+
+                return BinarySearchRecursive(sortedArray, middleNumberIndex + 1, maxNumberIndex, desiredNumber);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SearchingAlgorithms/SearchingAlgorithms/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using SearchingAlgorithms;
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int? desiredNumberIndex = SearchingAlgorithm.BinarySearch(array, 5);
+
+string output = null;
+
+if (desiredNumberIndex.HasValue)
+{
+    output = $"The number {array[desiredNumberIndex.Value]} is found on index {desiredNumberIndex.Value}";
+}
+else
+{
+    output = "The number is not found in the array!";
+}
+
+Console.WriteLine(output);
