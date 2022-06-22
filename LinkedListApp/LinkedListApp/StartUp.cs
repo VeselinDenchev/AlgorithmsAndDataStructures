683bc1d380231448a3f65b322429e81751751d19
VeselinDenchev <den4ev01@gmail.com>
2022-06-22 03:24:44 +0300

    Add LinkedList app

---
+++ LinkedListApp/LinkedListApp/StartUp.cs
@@ -0,0 +1,15 @@
+﻿LinkedList<int> linkedList = new LinkedList<int>();
+for (int i = 1; i <= 10; i++)
+{
+    linkedList.AddLast(i);
+}
+
+for (LinkedListNode<int> node = linkedList.First; node != null; node = node.Next)
+{
+     node.Value++;
+}
+
+foreach (int number in linkedList)
+{
+    Console.WriteLine(number);
+}
